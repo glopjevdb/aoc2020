ada35d25b805c74bf1cb0750cb7b736dc6769f8e
glopjevdb <91670760+glopjevdb@users.noreply.github.com>
2021-11-28 19:42:46 +0100

    finished day 2

--- src/opdracht2.rs
+++ src/opdracht2.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 pub fn run() {
 
@@ -14,6 +15,10 @@ pub fn run() {
     //println!("File contents: \n \n {}", contents);    werkt
 
     contents = contents.to_string();
+
+    let mut valid = 0;
+    let mut teller2 = 0;
+
 
     let vect: Vec<&str> = contents.lines().collect();
 
@@ -29,6 +34,7 @@ pub fn run() {
         let b = chars[1];
         let mut c = 0;
         let mut d = ' ';
+        let mut start = 0;
 
         let mut tweegetallen = false;
 
@@ -44,14 +50,16 @@ pub fn run() {
             tweegetallen = true;
         }
 
-        let mut kar: char = ' ';
+        let mut kar: char;
 
         if d == ' ' {
             if tweegetallen == false {
             kar = chars[4];
+            start = 6;
             } 
             else {
                 kar = chars[5];
+                start = 7;
             }
         }
 
@@ -61,17 +69,67 @@ pub fn run() {
 
             if tweegetallen == false {
                 kar = chars[5];
+                start = 7;
             }
             else {
                 kar = chars[6];
+                start = 8;
             }
         }
 
 
-        println!("{}-{}: {}", a, c, kar);
+        //println!("{}-{}: {}", a, c, kar);
 
         let min = a;
         let max = c;
+
+        let mut hm: HashMap<char, i32> = HashMap::new();
+
+        for i in start..chars.len() {
+            if hm.contains_key(&chars[i]) {
+                *hm.get_mut(&chars[i]).unwrap() += 1;
+            }
+
+            else {
+                hm.insert(chars[i], 1);
+            }
+        }
+
+        //println!("{:?}", hm);
+
+        if hm.contains_key(&kar) && hm[&kar] <= max && hm[&kar] >= min {
+            //println!("{:?}", x);
+            valid += 1;
+        }
+
+
+
+        let mut pos1 = false;
+        let mut pos2 = false;
+
+        if chars[start + min as usize] == kar {
+            pos1 = true;
+        }
+
+        if chars[start + max as usize ] == kar {
+            pos2 = true;
+        }
+
+        //println!("{} {}", chars[start + min as usize], chars[start + max as usize]);
+
+        if pos1 && !pos2 {
+            teller2 += 1;
+        }
+
+        if !pos1 && pos2 {
+            teller2 += 1;
+        }
+
     }
+
+    println!("{}", valid);
+    println!("OPDRACHT 2  ----------------------------------------------");
+    println!("{}", teller2);
+
 }
 
